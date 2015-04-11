9b50e9a7457cea8882c8863855783ebfb5bbf1c5
bsc9718 <bsc9718@rit.edu>
2015-04-11 11:51:20 -0400

    initial commit for review functionality. controller methods, views, and models for creating and viewing a review.

---
+++ Foodie/Foodie/Controllers/ReviewController.cs
@@ -0,0 +1,116 @@
+﻿using Foodie.Models;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Foodie.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["FoodieDatabase"].ConnectionString;
+        // GET: Review
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: create Review
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: create Review
+        [HttpPost]
+        public ActionResult Create(Foodie.Models.CreateReviewModel model)
+        {
+            Review review = CreateReview(model);
+            return RedirectToAction("Details", review);
+        }
+
+        //GET: review details page
+        public ActionResult Details(Foodie.Models.Review model)
+        {
+            return View(model);
+        }
+
+        /// <summary>
+        /// Create a Review
+        /// </summary>
+        /// <param name="?"></param>
+        /// <returns> the created Review model</returns>
+        private Review CreateReview(CreateReviewModel crModel)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                using (NpgsqlCommand command = conn.CreateCommand())
+                {
+                    conn.Open();
+                    Review review = new Review();
+                    int reviewId = 0;
+                    //command.CommandText = string.Format(CultureInfo.InvariantCulture, "select max(UserId) from \"{0}\"", "Reviews");
+                    //command.Prepare();
+                    //reviewId = Convert.ToInt32(command.ExecuteScalar());
+                    //incrementing pid
+                    //reviewId = reviewId + 1;
+
+
+
+                    //grab the user who's currently logged in
+                    //Guid personId = (Guid)Session["pId"];
+                    review.ReviewId = reviewId;
+                    //review.UserId = personId;
+                    review.ReviewText = crModel.ReviewText;
+                    review.RestaurantName = crModel.RestaurantName;
+                    review.RestaurantId = 0;
+                    review.DatePosted = DateTime.Now;
+                    using (NpgsqlCommand comm = conn.CreateCommand())
+                    {
+                        comm.CommandText = string.Format(CultureInfo.InvariantCulture,
+                            "INSERT INTO \"Reviews\" (\"ReviewId\", \"ReviewText\", \"AverageRating\", \"DatePosted\", \"UserId\", \"RestaurantId\") Values (@ReviewId, @ReviewText, @AverageRating, @DatePosted, @UserId, @RestaurantId)");
+                        comm.Parameters.Add("@ReviewId", NpgsqlDbType.Integer).Value = review.ReviewId;
+                        comm.Parameters.Add("@ReviewText", NpgsqlDbType.Text).Value = review.ReviewText;
+                        comm.Parameters.Add("@AverageRating", NpgsqlDbType.Real).Value = 0.0;
+                        comm.Parameters.Add("@DatePosted", NpgsqlDbType.Date).Value = review.DatePosted;
+                        //comm.Parameters.Add("@UserId", NpgsqlDbType.Char).Value = review.UserId;
+                        comm.Parameters.Add("@UserId", NpgsqlDbType.Char).Value = "test";
+                        comm.Parameters.Add("@RestaurantId", NpgsqlDbType.Integer).Value = review.RestaurantId;
+                        try
+                        {
+                            comm.Prepare();
+                            if (comm.ExecuteNonQuery() > 0)
+                            {
+                                Console.WriteLine("Success");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Failure");
+                            }
+
+                        }
+                        catch (NpgsqlException e)
+                        {
+                            Trace.WriteLine(e.ToString());
+                        }
+                        finally
+                        {
+                            if (conn != null)
+                            {
+                                conn.Close();
+                            }
+                        }
+
+                    }
+
+                    return review;
+                }
+            }
+        }
+    }
+}
---
+++ Foodie/Foodie/Models/ReviewModels.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Foodie.Models
+{
+
+    [Table("Reviews", Schema = "Public")]
+    public class Review
+    {
+        [Key]
+        [Column("ReviewId")]
+        public int ReviewId { get; set; }
+
+        [DataType(DataType.Text)]
+        [Column("ReviewText")]
+        public string ReviewText { get; set; }
+        
+        [Column("AverageRating")]
+        public float AverageRating { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Column("DatePosted")]
+        public DateTime DatePosted { get; set; }
+
+        [ForeignKey("Users")]
+        [Column("PostedBy")]
+        public Guid UserId { get; set; }
+
+        [ForeignKey("Restauraunts")]
+        [Column("RestaurantId")]
+        public int RestaurantId { get; set; }
+
+        [ForeignKey("Restauraunts")]
+        [Column("RestaurantId")]
+        public string RestaurantName { get; set; }
+
+
+
+    }
+    
+    [Table("Reviews", Schema = "Public")]
+    public class CreateReviewModel
+    {
+        [Required]
+        [Display(Name = "Restaurant")]
+        public string RestaurantName { get; set; }
+
+        [Required]
+        [Display(Name = "Review Text")]
+        [DataType(DataType.MultilineText)]
+        public string ReviewText { get; set; }
+    }
+}
