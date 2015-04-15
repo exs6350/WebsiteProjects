ad79814c83522ec17024bc4dba769368ee0f397e
dxo1613 <dxo1613@rit.edu>
2015-04-15 00:00:06 -0400

    ProfileController added to display simple Profile for now. Profile still incomplete

---
+++ Foodie/Foodie/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Foodie.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
